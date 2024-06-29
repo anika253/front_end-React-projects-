c288190c7c48b0f7ae94582729853128280a742a
anikas <22bec020@nith.ac.in>
2024-06-29 08:39:03 +0530

    yes

---
+++ bgchanger/bgChanger/src/App.jsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("olive");
+
+  return (
+    <>
+      <div
+        className="w-full h-screen duration-200"
+        style={{
+          backgroundColor: color,
+        }}
+      >
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-1 py-2 rounded-7xl">
+            <button
+              onClick={() => setColor("red")}
+              className="outline-none px-4 rounded-full full py-1 shadow-lg"
+              style={{ backgroundColor: "red" }}
+            >
+              Red
+            </button>
+            <button
+              onClick={() => setColor("green")}
+              className="outline-none px-4 rounded-full  py-1 shadow-lg"
+              style={{ backgroundColor: "green" }}
+            >
+              Green
+            </button>
+            <button
+              onClick={() => setColor("pink")}
+              className="outline-none px-4 rounded full  py-1 shadow-lg"
+              style={{ backgroundColor: "pink" }}
+            >
+              Pink
+            </button>
+            <button
+              onClick={() => setColor("blue")}
+              className="outline-none px-4 rounded full  py-1 shadow-lg"
+              style={{ backgroundColor: "blue " }}
+            >
+              blue
+            </button>
+            <button
+              onClick={() => setColor("orange ")}
+              className="outline-none px-4 rounded full  py-1 shadow-lg"
+              style={{ backgroundColor: "orange " }}
+            >
+              Orange
+            </button>
+            <button
+              onClick={() => setColor("brown")}
+              className="outline-none px-4 rounded full  py-1 shadow-lg"
+              style={{ backgroundColor: "brown" }}
+            >
+              Brown
+            </button>
+            <button
+              onClick={() => setColor("magenta")}
+              className="outline-none px-4 rounded full  py-1 shadow-lg"
+              style={{ backgroundColor: "magenta" }}
+            >
+              magenta
+            </button>
+            <button
+              onClick={() => setColor("magenta")}
+              className="outline-none px-4 rounded full  py-1 shadow-lg"
+              style={{ backgroundColor: "purple" }}
+            >
+              purple
+            </button>
+          </div>{" "}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
+// inline css ko inject krne k liye hm style k lie double brackets
+// use krte h {{ }}
